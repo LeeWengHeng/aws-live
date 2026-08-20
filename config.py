c74b644eaad28bf4335192fc45db837e4899deb7
LeeWengHeng <221234022+LeeWengHeng@users.noreply.github.com>
2026-08-20 15:40:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cjqyzqpuxp62.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leewengheng-employee"
+customregion = "us-east-1"
